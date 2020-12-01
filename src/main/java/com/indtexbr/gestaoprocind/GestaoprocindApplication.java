4ea4cac98f7d2546e2884225b73b400bde5cf7b0
Nathalia Amaral <nathalia.mbrtech@outlook.com>
2020-12-01 14:47:23 -0300

    First Commit

---
+++ src/main/java/com/indtexbr/gestaoprocind/GestaoprocindApplication.java
@@ -0,0 +1,13 @@
+package com.indtexbr.gestaoprocind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoprocindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoprocindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indtexbr/gestaoprocind/GestaoprocindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indtexbr.gestaoprocind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoprocindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
